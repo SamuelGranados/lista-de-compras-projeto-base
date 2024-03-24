10d8a12c138ddd0fa173f0090a8c366d13de755c
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2024-03-24 19:43:28 -0300

    alterando o valor do objeto

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ let listaDeItens = []
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
+const ulItens = document.getElementById("lista-de-itens")
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
     salvarItem()
+    mostrarItem()
 })
 
 function salvarItem() {
@@ -16,9 +18,38 @@ function salvarItem() {
         alert("Item já existe")
     } else {
         listaDeItens.push({
-            valor: comprasItem
+            valor: comprasItem,
+            checar: false
         })
     }
 
     console.log(listaDeItens)
+}
+
+function mostrarItem() {
+    ulItens.innerHTML = ''
+
+    listaDeItens.forEach((elemento, index) => {
+        ulItens.innerHTML += `
+        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+        `
+    })
+
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+    inputsCheck.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens[valorDoElemento].checar = evento.target.checked
+            console.log(listaDeItens[valorDoElemento].checar)
+        })
+    })
 }
