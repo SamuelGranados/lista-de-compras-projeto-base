32a71c1cf888d5ea2dafcbf05c0eebfd9827f20f
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2024-03-26 03:03:56 -0300

    identidicando indice do objeto

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = []
+let itemAEditar
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
@@ -53,6 +54,7 @@ function mostrarItem() {
             <input type="text" class="is-size-5" value="${elemento.valor}"></input>
         </div>
         <div>
+        <i class="fa-regular fa-floppy-disk is-clickable"></i><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
@@ -68,6 +70,26 @@ function mostrarItem() {
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
             mostrarItem()
+        })
+    })
+
+    const deletarObjetos = document.querySelectorAll(".deletar")
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens.splice(valorDoElemento, 1)
+            mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll(".editar")
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+            console.log(itemAEditar)
         })
     })
 }
